e0c25d007054344b8ea842b31069540180d0b87b
Tr3v0r_C <trevor.clarke62@gmail.com>
2017-10-15 15:47:52 -0400

    Creating code added getCoordinates()

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 window.onload = setup;
-var width, height;
+var width, height, coordinates;
 
 function setup() {
     canvas = document.getElementById("canvas");
     c = canvas.getContext("2d");
     sizeCanvas();
 
-    //draw();
-    //window.setInterval(draw, 16.6);
+    coordinates = getCoordinates("pizza", 10);
+
+    window.setInterval(draw, 16.6);
 }
 
 function draw() {
-  //Put main code / loop here
+  for (i = 0; i < coordinates.length; i++) {
+    var p = coordinates[i];
+    c.beginPath();
+    c.arc(p.x, p.y, 2, 0, 2*Math.PI);
+    c.fill();
+
+  }
 }
 
 function sizeCanvas() {
@@ -25,3 +32,23 @@ window.onresize = function() {
     sizeCanvas();
     draw();
 };
+
+function getCoordinates(text, step){
+  var values = [];
+  var data;
+  c.font = "300px Arial";
+  c.fillText(text, 150,300);
+  data = c.getImageData(0, 0, width, height).data;
+  c.clearRect(0, 0, width, height);
+  for (i = 0; i < height; i+=step) {
+      for (j = 4; j < width; j+=step) {
+          var a = i * width * 4 + j * 4;
+          if(data[a+3] !=0)
+          values.push({
+            x: j,
+            y: i
+          });
+      }
+  }
+  return values;
+}
